5802d09ed5b4420133bbfbdde54d187a6c215f6a
Jeremy Jones <jeremy.jones@cazoodle.com>
2010-12-20 08:05:55 -0600

    adding project files

---
+++ src/InvalidEarningsException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jjones
+ */
+public class InvalidEarningsException extends Exception{
+    public InvalidEarningsException(String message){
+        super(message);
+    }
+
+}
---
+++ src/InvalidTimeException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jjones
+ */
+public class InvalidTimeException extends Exception{
+    public InvalidTimeException(String message) {
+        super(message);
+    }
+
+}
